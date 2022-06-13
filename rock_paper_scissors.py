b751b09ac1518a4e615bd6a82a320ec185095ca6
Tabitha Ama Abeka <99736998+a-tae-bi@users.noreply.github.com>
2022-06-13 13:07:29 +0000

    Ensuring that the game restarts when there is a tie
    Fixed the arrangement of the loops to ensure that the program firsts checks if there is a tie before it proceeds with the other loops.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,47 +8,49 @@ print("Winning Rules of the Rock paper scissor game as follows: \n"
                                 + "Rock vs scissor->Rock wins \n"
                                 +"paper vs scissor->scissor wins \n")
 
-possible_actions = ["R", "P", "S"]
-
 print("Enter choice \n R for Rock, \n P for paper, and \n S for scissor \n")
-     
-# take the input from user
-user_action = input("User turn: ")
-user_action = user_action.upper()
 
-while(user_action not in possible_actions):
+play = True
+
+while play == True:
+    possible_actions = ["R", "P", "S"]
+    
+    # take the input from user
+    user_action = input("User turn: ")
+    user_action = user_action.upper()
+
+    while(user_action not in possible_actions):
         print("Invalid choice enterred")
         print("Enter choice \n R for Rock, \n P for paper, and \n S for scissor \n")
         user_action = input("User turn: ").upper()
     
-computer_action = random.choice(possible_actions)
-print(f"\nPlayer ({user_action}) : CPU ({computer_action})\n")
+    computer_action = random.choice(possible_actions)
+    print(f"\nPlayer ({user_action}) : CPU ({computer_action})\n")
 
-if user_action == "R":
-    if computer_action == "S":
-        print("Rock smashes scissors! You win!")
+    if(user_action == computer_action):
+        print(f"Both players selected {user_action}. It's a tie!")
+        play = True
+    elif user_action == "R":
+        if computer_action == "S":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! Computer wins.")
         print("\nThanks for playing")
-    else:
-        print("Paper covers rock! Computer wins.")
+        play = False
+    elif user_action == "P":
+        if computer_action == "R":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! Computer wins.")
         print("\nThanks for playing")
-elif user_action == "P":
-    if computer_action == "R":
-        print("Paper covers rock! You win!")
+        play = False
+    elif user_action == "S":
+        if computer_action == "P":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! Computer wins.")
         print("\nThanks for playing")
-    else:
-        print("Scissors cuts paper! Computer wins.")
-        print("\nThanks for playing")
-elif user_action == "S":
-    if computer_action == "P":
-        print("Scissors cuts paper! You win!")
-        print("\nThanks for playing")
-    else:
-        print("Rock smashes scissors! Computer wins.")
-        print("\nThanks for playing")
-elif(user_action == computer_action):
-    print(f"Both players selected {user_action}. It's a tie!")
-    print("Enter choice \n R for Rock, \n P for paper, and \n S for scissor \n")
-    user_action = input("User turn: ")
-    user_action = user_action.upper()
+        play = False
+    
 
     
